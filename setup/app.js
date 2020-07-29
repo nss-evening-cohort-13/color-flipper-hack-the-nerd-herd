eead4a23a9574c5cdcbbf20f408abf1b8e31fed6
Wanda <wmmcinturff@gmail.com>
2020-07-28 20:17:43 -0500

    created array of simple colors

---
+++ setup/app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+
+const simplePalette = [
+    {
+        color: 'beige',
+    },
+
+    {
+        color: 'salmon',
+    },
+
+    {
+        color: 'teal',
+    },
+
+    {
+        color: 'goldenrod',
+    
+    },
+
+    {
+        color: 'purple',
+    },
+
+    {
+        color: 'rust red',
+    },
+
+    {
+        color: 'chartreuse',
+    },
+
+    {
+        color: 'azure',
+    },
+
+    {
+        color: 'peach',    
+    },
+
+    {
+        color: 'aqua',
+    },
+]
